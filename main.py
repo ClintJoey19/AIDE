f351bf99491373b4875f13a2aa741e6c97c65ac6
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-02-20 11:08:36 +1100

    readme created

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pdf_reader.extractor import get_order_code
 
 if __name__ == "__main__":
-    pdf = "./pdf_folder/invoice-1.pdf"
+    pdf = "./pdf_folder/try.pdf"
     print(get_order_code(pdf))
