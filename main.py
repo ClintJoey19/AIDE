d4086e91159574276fa590f213c06bb9b3f9c8af
ClintJoey19 <llosalaclintjoey@gmail.com>
2024-02-21 16:26:26 +1100

    added ui elements

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from pdf_reader.extractor import get_order_code
 from automation.automate_data_entry import automate
 import sys
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QAction
+from PyQt6.QtCore import QSize, Qt, QFileInfo
 from PyQt6.QtWidgets import (
     QApplication,
     QCheckBox,
@@ -11,12 +10,14 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QWidget,
+    QFileDialog
 )
 
 class Main(QMainWindow):
     def __init__(self):
         super().__init__()
         self.app_title = "AIDE"
+        self.pdf_path = None
 
         self.setWindowTitle(self.app_title)
 
@@ -26,14 +27,29 @@ class Main(QMainWindow):
         title = QLabel(self.app_title)
         font = title.font()
         font.setPointSize(20)
+        font.setBold(True)
         title.setFont(font)
         title.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         box.addWidget(title)
 
+        # invoice input label
+        invoice_input = QLabel("Choose invoice (PDF): ")
+        font = invoice_input.font()
+        font.setPointSize(11)
+        invoice_input.setFont(font)
+        invoice_input.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        box.addWidget(invoice_input)
+
+        # invoice file input
+        self.invoice_file = QPushButton("Select a file")
+        self.invoice_file.setShortcut("Ctrl+O")
+        self.invoice_file.clicked.connect(self.open_invoice_pdf)
+        box.addWidget(self.invoice_file)
+
         # functionalities label
         func_label = QLabel("Choose automation function: ")
         font = func_label.font()
-        font.setPointSize(12)
+        font.setPointSize(11)
         func_label.setFont(font)
         func_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
         box.addWidget(func_label)
@@ -43,22 +59,27 @@ class Main(QMainWindow):
         box.addWidget(self.data_entry_cb)
 
         # action btn
-        action_btn = QPushButton("Start")
-        action_btn.clicked.connect(self.run)
-        box.addWidget(action_btn)
+        self.action_btn = QPushButton("Start")
+        self.action_btn.setShortcut("Ctrl+R")
+        self.action_btn.clicked.connect(self.run)
+        box.addWidget(self.action_btn)
 
         widget = QWidget()
         widget.setLayout(box)
+        self.setFixedSize(QSize(300, 200))
         self.setCentralWidget(widget)
 
     def run(self):
         if (self.data_entry_cb.isChecked()):
-            print("Checked")
+            self.action_btn.setEnabled(False)
             pdf = "./pdf_folder/try.pdf"
             codes = self.run_extract(pdf)
             automate(codes)
-        else:
-            print("Not Checked")
+            self.action_btn.setEnabled(True)
+
+    def open_invoice_pdf(self):
+        # todo: get pdf absolute path
+        print("pdf url")
 
     def run_extract(self, pdf):
         code = get_order_code(pdf)
@@ -74,7 +95,3 @@ def set_Ui():
 
 if __name__ == "__main__":
     set_Ui()
-
-    # pdf = "./pdf_folder/try.pdf"
-    # codes = get_order_code(pdf)
-    # automate(codes)
